2664eadc57dc07af9d6b2d8f20ce4972eda1249d
Keith Webster <keith.p.webster@gmail.com>
2017-03-25 16:50:33 -0700

    Coursera wk 3 cachmean
    makeCacheMatrix
    cacheSolve

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cachematrix.R Week 3 Programming Assignment 2
 
-## Write a short comment describing this function
+## Creates a matrix object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-
+		inv <- NULL
+		set <- function(y)  {
+				x <<- y
+				inv <<- NULL
+		}
+		get <- function() x
+		setinv <- function(inv) inv <<- inverse
+		getinv <- function() inv
+		list(set = set, get = get, setinv = setinv, getinv = getinv)
+		
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of matrix returned by makeCacheMatrix. If already calculated it will return answer from cache rather than recalculate and saving processing time
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv))  {
+        	message("getting cached data")
+        	return(inv)
+        }
+        mat.data <- x$get
+        inv <- solve(mat.data, ...)
+        x$setinv(inv)
+        return(inv)
 }
